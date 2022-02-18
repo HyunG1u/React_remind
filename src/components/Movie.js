cdef0128fe0397e996df2f853cbf6892d14ebf9d
HyunG1u <58351289+HyunG1u@users.noreply.github.com>
2022-02-18 16:01:20 +0900

    feat: css modules

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,20 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-const Movie = ({ id, coverImg, title, genres, summary }) => {
+const Movie = ({ id, coverImg, title, year, genres, summary }) => {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <h2 className={styles.movie__title}>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <ul>
+      <ul className={styles.movie__genres}>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
       </ul>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
     </div>
   );
 };
