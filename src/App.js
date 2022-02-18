4c268be9c140088f2df5b6acde0d7ed813e908f0
HyunG1u <58351289+HyunG1u@users.noreply.github.com>
2022-02-18 15:44:47 +0900

    feat: add movie pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Movie from "./components/Movie";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
